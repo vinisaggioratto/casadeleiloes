accba1774942b2f6bb8b5e095e59a224d7d413c2
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-06-30 16:52:26 -0300

    Criado método que faz a verificação e alteração do status de um produto para vendido

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -24,8 +24,6 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     ProdutosDTO produto = new ProdutosDTO();
 
-    private final String[] tableColumns = {"Id", "Nome", "Valor", "Status"};
-
     public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
         conn = new conectaDAO().connectDB();
 
@@ -78,8 +76,8 @@ public class ProdutosDAO {
             return null;
         }
     }
-    
-        public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
 
         String sql = "select id, nome, valor, status from produtos where status  = 'Vendido'";
 
@@ -103,6 +101,55 @@ public class ProdutosDAO {
             return listagem;
         } catch (SQLException e) {
             return null;
+        }
+    }
+
+    public void venderProduto(Integer id) {
+        String sql = "select id, nome, valor, status from produtos where id = ?";
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+                
+                if (produto.getStatus().equals("Vendido")) {
+                    JOptionPane.showMessageDialog(null, "Produto não disponível "
+                            + "para venda.");
+                } else {
+                    System.out.println("Dentro do else update venda");
+                    //listagem.add(produto);
+                    String sqlUpdate = "update produtos set "
+                            + "nome = ?, valor = ?, status = ? where id = ?";
+                    prep = conn.prepareStatement(sqlUpdate);
+
+                    prep.setString(1, produto.getNome());
+                    prep.setInt(2, produto.getValor());
+                    prep.setString(3, "Vendido");
+                    prep.setInt(4, produto.getId());
+
+                    int executado = prep.executeUpdate();
+                    if (executado > 0) {
+                        JOptionPane.showMessageDialog(null, "Produto "
+                                + "alterado com sucesso.");
+                    }
+                }
+            }
+            resultset.close();
+            //return listagem;
+        } catch (SQLException e) {
+            //return null;
+            JOptionPane.showMessageDialog(null, "Erro ao processar venda.");
+            System.out.println("Erro ao processar a venda." + e);
         }
     }
 }
